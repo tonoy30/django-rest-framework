84d0d8f2bde605111052f7543fc65f0d7574c5dd
tonoy30 <tonoy30@student.sust.edu>
2019-02-22 01:15:04 +0600

    generics views

--- RestReviewProject/restReview/urls.py
+++ RestReviewProject/restReview/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('reviews/', views.ListReview.as_view(), name='reviews'),
     path('courses/', views.ListCreateCourse.as_view(), name='courses'),
+    path('courses/<int:pk>/', views.RetriveUpdateDestroyCourse.as_view(),
+         name='course_detail'),
+    path('reviews/<int:pk>/', views.RetriveUpdateDestroyReview.as_view(),
+         name='review_detail'),
 ]
--- RestReviewProject/restReview/views.py
+++ RestReviewProject/restReview/views.py
@@ -1,31 +1,30 @@
 from . import serializers
 from . import models
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from django.shortcuts import render
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    courses = models.Course.objects.all()
+    return render(request, 'home.html', {'course_list': courses})
 
 
-class ListCreateCourse(APIView):
-    def get(self, request, format=None):
-        courses = models.Course.objects.all()
-        serializer = serializers.CouseSerializer(courses, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = serializers.CouseSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CouseSerializer
 
 
-class ListReview(APIView):
-    def get(self, request, format=None):
-        reviews = models.Review.objects.all()
-        serializer = serializers.ReviewSerializer(reviews, many=True)
-        return Response(serializer.data)
+class RetriveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CouseSerializer
+
+
+class ListReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+
+class RetriveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
